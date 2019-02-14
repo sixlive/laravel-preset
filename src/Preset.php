b4a255e89d2fd7dbdceed159bcdb0bbb0d58dfec
TJ Miller <sixlive@users.noreply.github.com>
2019-02-14 06:50:43 -0500

    Package prompts (#2)
    * Prompt for optional packages
    * Prompt to remove the preset as you really only run it once
    * Update the docker run script to check for the Bouncer seeder first.

--- src/Preset.php
+++ src/Preset.php
@@ -2,91 +2,147 @@
 
 namespace sixlive\LaravelPreset;
 
+use Illuminate\Support\Collection;
 use Illuminate\Filesystem\Filesystem;
 use sixlive\DotenvEditor\DotenvEditor;
 use Illuminate\Foundation\Console\Presets\Preset as BasePreset;
 
 class Preset extends BasePreset
 {
-    public static function install($command)
+    protected $command;
+    protected $options = [];
+
+    public function __construct($command)
     {
-        $command->task('Install composer dependencies', function () {
-            return static::updateComposerPackages();
-        });
-
-        $command->task('Install composer dev-dependencies', function () {
-            return static::updateComposerDevPackages();
-        });
-
-        $command->task('Publish stubs', function () {
-            static::publishStubs();
-        });
-
-        $command->task('Update ENV files', function () {
-            static::updateEnvFile();
-        });
-
-        $command->task('Regenerate composer autoload file', function () {
-            static::runCommand('composer dumpautoload');
-        });
+        $this->command = $command;
     }
 
-    public static function updateComposerPackages()
+    public static function install($command)
     {
-        $packages = [
+        $preset = new static($command);
+
+        $preset->run();
+    }
+
+    public function run()
+    {
+        $this->gatherOptions();
+
+        if (!empty($this->options['packages'])) {
+            $this->command->task('Install composer dependencies', function () {
+                return $this->updateComposerPackages();
+            });
+        }
+
+        $this->command->task('Install composer dev-dependencies', function () {
+            return $this->updateComposerDevPackages();
+        });
+
+        $this->command->task('Publish stubs', function () {
+            $this->publishStubs();
+        });
+
+        $this->command->task('Update ENV files', function () {
+            $this->updateEnvFile();
+        });
+
+        $this->command->task('Regenerate composer autoload file', function () {
+            $this->runCommand('composer dumpautoload');
+        });
+
+        if ($this->options['remove_after_install']) {
+            $this->command->task('Remove sixlive/laravel-preset', function () {
+                $this->runCommand('composer remove sixlive/laravel-preset');
+                $this->runCommand('composer dumpautoload');
+            });
+        }
+    }
+
+    private function gatherOptions()
+    {
+        $this->command->line('');
+        $this->command->warn('Optional packages:');
+        $this->options['packages'] = $this->promptForPackagesToInstall();
+
+        $this->command->line('');
+        $this->command->warn('Misc. options:');
+        $this->options['remove_after_install'] = $this->command->confirm('Remove sixlive/laravel-preset after install?');
+    }
+
+    private function promptForPackagesToInstall()
+    {
+        $possiblePackages = [
             'bensampo/laravel-enum',
             'silber/bouncer:v1.0.0-rc.4',
             'sentry/sentry-laravel',
             'dyrynda/laravel-model-uuid',
         ];
 
-        static::runCommand('composer require '. implode(' ', $packages));
+        return Collection::make($possiblePackages)
+            ->filter(function ($package) {
+                return $this->command->confirm("Install {$package}?", true);
+            })
+            ->toArray();
     }
 
+    private function updateComposerPackages()
+    {
+        $this->runCommand(
+            'composer require'.implode(' ', $this->options['packages'])
+        );
+    }
 
-    public static function updateComposerDevPackages()
+    private function updateComposerDevPackages()
     {
         $packages = [
             'sempro/phpunit-pretty-print',
             'sensiolabs/security-checker',
         ];
 
-        static::runCommand('composer require --dev '. implode(' ', $packages));
+        $this->runCommand('composer require --dev '. implode(' ', $packages));
     }
 
-    public static function publishStubs()
+    private function publishStubs()
     {
         copy(__DIR__.'/stubs/Model.php', app_path('Model.php'));
         copy(__DIR__.'/stubs/phpunit.xml', base_path('phpunit.xml'));
+        copy(__DIR__.'/stubs/.php_cs', base_path('.php_cs'));
         copy(__DIR__.'/stubs/docker-compose.yml', base_path('docker-compose.yml'));
-        copy(__DIR__.'/stubs/BouncerSeeder.php', database_path('seeds/BouncerSeeder.php'));
+
+        if (in_array('silber/bouncer:v1.0.0-rc.4', $this->options['packages'])) {
+            copy(__DIR__.'/stubs/BouncerSeeder.php', database_path('seeds/BouncerSeeder.php'));
+        }
+
         tap(new Filesystem, function ($files) {
             $files->copyDirectory(__DIR__.'/stubs/.docker', base_path('.docker'));
         });
     }
 
-    public static function updateEnvFile()
+    private function updateEnvFile()
     {
-        $editor = new DotenvEditor;
-        $editor->load(base_path('.env'));
-        $editor->set('DB_PORT', '3307');
-        $editor->heading('Docker');
-        $editor->set('DOCKER_WEB_PORT', '8080');
-        $editor->set('DOCKER_MYSQL_PORT', '3307');
-        $editor->save();
+        tap(new DotenvEditor, function ($editor) {
+            $editor->load(base_path('.env'));
+            $editor->set('DB_PORT', '3307');
+            $editor->heading('Docker');
+            $editor->set('DOCKER_WEB_PORT', '8080');
+            $editor->set('DOCKER_MYSQL_PORT', '3307');
+            $editor->save();
+        });
 
-        $editor = new DotenvEditor;
-        $editor->load(base_path('.env.example'));
-        $editor->set('DB_PORT', '3307');
-        $editor->set('SENTRY_DSN', '');
-        $editor->addEmptyLine();
-        $editor->heading('Docker');
-        $editor->set('DOCKER_WEB_PORT', '8080');
-        $editor->set('DOCKER_MYSQL_PORT', '3307');
-        $editor->save();
+        tap(new DotenvEditor, function ($editor) {
+            $editor = new DotenvEditor;
+            $editor->load(base_path('.env.example'));
+            $editor->set('DB_PORT', '3307');
+            $editor->set('SENTRY_DSN', '');
+            $editor->addEmptyLine();
+            $editor->heading('Docker');
+            $editor->set('DOCKER_WEB_PORT', '8080');
+            $editor->set('DOCKER_MYSQL_PORT', '3307');
+            $editor->save();
+        });
     }
 
-    private static function runCommand($command)
+    private function runCommand($command)
     {
         return exec(sprintf('%s 2>&1', $command));
     }
